ade4962cae9e996c93bacb0b3110e66de92194ea
ArpitSingh73 <arpitsingh73073@gmail.com>
2024-01-04 09:32:36 +0530

    Project Initialized.(4th jan)

---
+++ contracts/Cookie.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >= 0.5.0 < 0.9.0;
+
+
+contract cookies{
+   struct Memo{
+     string name;
+     string message;
+     uint timestamp;
+     address from;
+    }
+
+    Memo[] memos;
+    address payable owner;
+
+    constructor(){
+        owner = payable( msg.sender);
+    }
+
+    function buyCookies(string memory name, string memory message) public payable {
+            require(msg.value > 0, "Please pay me meore than 0eth");
+            owner.transfer(msg.value);
+            memos.push(Memo(name, message, block.timestamp, msg.sender));
+    }
+
+    function get() view public returns(Memo[] memory){
+            return memos;
+    }
+}

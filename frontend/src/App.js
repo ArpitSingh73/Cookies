5a7fe592ad63cf4c10885babbc699e87058c4eeb
ArpitSingh73 <arpitsingh73073@gmail.com>
2024-01-20 11:30:07 +0530

    connection with blockchain completed.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+// import { InfuraProvider } from "ethers/providers";
+// import { InfuraProvider } from "ethers"
+
+// The pkg.exports provides granular access
+import { InfuraProvider } from "ethers/providers"
+
+// const { ethers } = require("hardhat");
+
+import abi from "./cookies.json";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+
+  useEffect(() => {
+    const connectWallet = async () => {
+      const contractAdd = "0x61e1c3180372de8d20ac4bb55525c05c7ba86e25";
+      const contractABI = abi.abi;
+
+      try {
+        const { ethereum } = window;
+        if (ethereum) {
+          const account = await ethereum.request({ method: "eth_requestAccounts" });
+        }
+
+        // const provider = new ethers.providers.Web3Provider(ethereum);
+                // console.log(InfuraProvider);
+
+        const provider = new ethers.BrowserProvider(ethereum);
+        const signer = await provider.getSigner();
+        const contract = new ethers.Contract(contractAdd, contractABI, signer);
+        setState({
+          provider,
+          signer,
+          contract,
+        });
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    connectWallet();
+  }, []);
+  
+console.log(state)
+  return <div className="App">
+    lkjgyhdbjnkm hjtfdhcfgvhbmkl,
+  </div>;
 }
 
 export default App;
